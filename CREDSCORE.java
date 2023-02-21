4a1d38b64e9ec79ec3c029d5dd251be2ffeb6858
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-21 21:05:19 +0530

    Add files via upload

---
+++ CREDSCORE.java
@@ -0,0 +1,17 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int x = s.nextInt();
+		String res = x>=750 ? "YES" : "NO";
+		System.out.println(res);
+	}
+}
