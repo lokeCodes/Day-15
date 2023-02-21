957ed95939d55365bee8c5b036cac31a69be4d40
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-21 21:28:56 +0530

    Add files via upload

---
+++ INTEST.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        int k = s.nextInt();
+        int sum=0;
+        while(t-- > 0){
+            int t1 = s.nextInt();
+            if(t1%k==0){
+                sum++;
+            }
+        }
+        System.out.println(sum);
+	}
+}
