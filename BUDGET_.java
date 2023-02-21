a6de0fceb7b339726b1519b621db0544fb1618e2
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-21 21:01:31 +0530

    Add files via upload

---
+++ BUDGET_.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-- > 0){
+		    int x = s.nextInt();
+		    int y = 30*(s.nextInt());
+		    String res = (y<=x) ? "YES" : "NO ";
+		    System.out.println(res);
+		}
+	}
+}
