eceb24b5ca52d0eb22ad6fea86c99a5dbd0c1fc9
lokeCodes <125755158+lokeCodes@users.noreply.github.com>
2023-02-21 21:06:21 +0530

    Update CREDSCORE.java

--- CREDSCORE.java
+++ CREDSCORE.java
@@ -1,5 +1,8 @@
 /* package codechef; // don't place package name! */
 
+// https://www.codechef.com/problems/CREDSCORE
+// Question Link^^^
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
